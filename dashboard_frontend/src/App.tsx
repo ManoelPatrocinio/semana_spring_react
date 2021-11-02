8bf0104f1348a4cad84eb52a017838addf9e151f
ManoelPatrocinio <manoelpatrocinio99@gmail.com>
2021-11-02 00:42:44 -0300

    Basic static components

--- dashboard_frontend/src/App.tsx
+++ dashboard_frontend/src/App.tsx
@@ -1,8 +1,15 @@
+import { DataTable } from "components/DataTable";
+import { Footer } from "components/Footer";
+import { NavBar } from "components/NavBar";
 function App() {
   return (
-    <div>
-      <h1>ola mundo</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
